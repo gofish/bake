gofish/bake

commit e913258b9377e2578f544d41e269ad53c4ed0d6f

fix deprecated syntax
The following error was occuring on at least Ubuntu 14.10:

    Makefile:64: *** mixed implicit and normal rules: deprecated syntax
    make: *** No rule to make target 'targets', needed by 'all'.  Stop.

De-mixing was applied.

bake.mk
@@ -61,7 +61,8 @@ Overrides.mk: ;
 
 ### Delegate all unspecified goals to a sub-make
 #
-release debug profile coverage %::     force
+release debug profile coverage::     force
+%:: force
 	$(eval export BUILD_TYPE)
 	# Execute this Makefile from a build-specific subdirectory
 	DEST=$${DEST:-"build/$(BUILD_TYPE)"}; \

commit 018f10a2a431ae0cb7f785b0a407be4f5ebc2cf4

fix named release targets
Commit e913258 introduced a regression disabling targets which specify
the build type directly using the name of a top-level goal. To maintain
the deprecated syntax fix but restore this feature we duplicate the sub-
make rule structure for the two cases.

bake.mk
@@ -26,14 +26,6 @@ BUILD_TYPE := debug
 endif
 endif
 
-### Build type as goal overrides environment
-#
-release:        BUILD_TYPE := release
-debug:          BUILD_TYPE := debug
-profile:        BUILD_TYPE := profile
-coverage:       BUILD_TYPE := coverage
-.PHONY: release debug profile coverage
-
 ### Export search path for source files
 #
 # Override this to change the source root directory
@@ -59,9 +51,20 @@ MAKEFILE       := $(firstword $(MAKEFILE_LIST))
 $(MAKEFILE): ;
 Overrides.mk: ;
 
+### Delegate release goals to a sub-make and override BUILD_TYPE
+#
+release debug profile coverage:: force
+	# Goal '$@' overrides current build type '$(BUILD_TYPE)'
+	$(eval export BUILD_TYPE=$@)
+	# Execute this Makefile from a build-specific subdirectory
+	DEST=$${DEST:-"build/$(BUILD_TYPE)"}; \
+	    echo Building to $$DEST && \
+	    mkdir -p "$$DEST" && \
+	    $(MAKE) -C "$$DEST" -I $(CURDIR) -I $(VPATH) -f $(abspath $(MAKEFILE)) --no-print-directory $@
+.PHONY: release debug profile coverage
+
 ### Delegate all unspecified goals to a sub-make
 #
-release debug profile coverage::     force
 %:: force
 	$(eval export BUILD_TYPE)
 	# Execute this Makefile from a build-specific subdirectory